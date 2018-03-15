454b14666b37d71dbb103957d95c287ccf8eddf4
Benjamin Bowes <bbowes999@gmail.com>
2018-03-15 10:23:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,4 +15,6 @@ def speak_to_grandma(string)
   if string == !string.upcase
     puts "HUH?! SPEAK UP, SONNY!"
     elsif string == string.upcase
-    puts 
+    puts "NO, NOT SINCE 1938!"
+  else
+    string == 
